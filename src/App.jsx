2fa66264b671aa4bcb27303c2b31aa4b69db3ee3
2405-007-dk <danakengessova@gmail.com>
2024-06-27 11:10:17 +0100

    add fucntionality to arrows for numbers

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,27 @@
 import { englishWords, russianWords } from "./components/LanguageData";
-import React from 'react'
+import React from "react";
 
 function App() {
+    const [count, setCount] = React.useState(1);
 
-    const[count, setCount] = React.useState(1)
+    function prevNumber() {
+        if (count >= 1) {
+            setCount((count) => count - 1);
+        }
+    }
+
+    function nextNumber() {
+        if (count < 10) {
+            setCount((count) => count + 1);
+        }
+    }
 
     return (
         <div>
             <h1>Language Practice - Counting</h1>
             <div className="buttons">
-                <button>⬅️</button>
-                <button>➡️</button>
+                <button onClick={prevNumber}>⬅️</button>
+                <button onClick={nextNumber}>➡️</button>
                 <button>Show/hide English</button>
                 <button>Show/hide Russian</button>
                 <button>Random (English)</button>
