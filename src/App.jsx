ab5fb986ea25654ac7f73ab5cba8e37d39aae2e5
2405-007-dk <danakengessova@gmail.com>
2024-06-27 11:44:40 +0100

    add final changes with random functionality

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,24 @@ import React from "react";
 
 function App() {
     const [count, setCount] = React.useState(1);
-    const [showEnglish, setShowEnglish] = React.useState(true)
-    const [showRussian, setShowRussian] = React.useState(true)
+    const [showEnglish, setShowEnglish] = React.useState(true);
+    const [showRussian, setShowRussian] = React.useState(true);
+
+    function random0to10() {
+        return Math.floor(Math.random() * englishWords.length);
+    }
+
+    function random0to10Eng() {
+        setCount(random0to10());
+        setShowEnglish(true);
+        setShowRussian(false);
+    }
+
+    function random0to10Rus() {
+        setCount(random0to10());
+        setShowEnglish(false);
+        setShowRussian(true);
+    }
 
     function prevNumber() {
         if (count >= 1) {
@@ -19,11 +35,11 @@ function App() {
     }
 
     function toggleShowEnglish() {
-        setShowEnglish((prevValue) => !prevValue)
+        setShowEnglish((prevValue) => !prevValue);
     }
 
     function toggleShowRussian() {
-        setShowRussian((prevValue) => !prevValue)
+        setShowRussian((prevValue) => !prevValue);
     }
 
     return (
@@ -32,14 +48,14 @@ function App() {
             <div className="buttons">
                 <button onClick={prevNumber}>⬅️</button>
                 <button onClick={nextNumber}>➡️</button>
-                <button onClick={toggleShowEnglish} >Show/hide English</button>
-                <button onClick={toggleShowRussian} >Show/hide Russian</button>
-                <button>Random (English)</button>
-                <button>Random (Russian)</button>
+                <button onClick={toggleShowEnglish}>Show/hide English</button>
+                <button onClick={toggleShowRussian}>Show/hide Russian</button>
+                <button onClick={random0to10Eng}>Random (English)</button>
+                <button onClick={random0to10Rus}>Random (Russian)</button>
             </div>
             <div className="numbers">
-                <h2>{showEnglish ? englishWords[count]: '?'}</h2>
-                <h2>{showRussian ? russianWords[count]: '?'}</h2>
+                <h2>{showEnglish ? englishWords[count] : "?"}</h2>
+                <h2>{showRussian ? russianWords[count] : "?"}</h2>
             </div>
         </div>
     );
